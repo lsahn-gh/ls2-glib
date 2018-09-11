e36ce43a28766a56878262cb4dfe73f8b807f6d5
Yi-Soo An <yisooan@gmail.com>
2018-09-11 12:14:41 +0900

    Add public header
    glus2.h is the public header for including.
    Developers can only include it.
    
    Signed-off-by: Yi-Soo An <yisooan@gmail.com>

---
+++ src/glus2.h
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2018 Yi-Soo An <yisooan@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef GLUS2_MAIN_H
+#define GLUS2_MAIN_H
+
+#define __GLUS2_HEADER_INCLUDE__
+
+#include <glib.h>
+#include <luna-service2/lunaservice.h>
+
+#include "glus2-boxed-types.h"
+#include "glus2-call.h"
+#include "glus2-general.h"
+#include "glus2-handle.h"
+#include "glus2-log.h"
+#include "glus2-macros.h"
+#include "glus2-mainloop.h"
+#include "glus2-message.h"
+#include "glus2-signal.h"
+#include "glus2-subscription.h"
+
+#undef __GLUS2_HEADER_INCLUDE__
+
+#endif
