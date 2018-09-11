ca2506f587f202c97d7427fa5ce92d6c985133cf
Yi-Soo An <yisooan@gmail.com>
2018-09-11 11:49:03 +0900

    macros: Implement useful macros
    Signed-off-by: Yi-Soo An <yisooan@gmail.com>

---
+++ src/glus2-macros.h
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2018 Yi-Soo An <yisooan@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef GLUS2_MACROS_H
+#define GLUS2_MACROS_H
+
+#if !defined (__GLUS2_HEADER_INCLUDE__) && !defined (GLUS2_COMPILATION)
+#   error "Only <glus2.h> can be included directly!"
+#endif
+
+#include <glib.h>
+
+G_BEGIN_DECLS
+
+/* Used for GApplication and Luna-Service2 */
+#define LIBRARY_ID              "glus2"
+
+#define PMLOG_CONTEXT_NAME      LIBRARY_ID
+#define ERROR_MESSAGE_ID        LIBRARY_ID
+
+#define NONE    (void)0         /* Its type is void */
+
+/* --- Useful macros --- */
+#define     glus2_ret_false_if_fail(expr)      g_return_val_if_fail(expr, FALSE)
+#define     glus2_ret_null_if_fail(expr)       g_return_val_if_fail(expr, NULL)
+
+#define     glus2_ret_val_if_fail_autodef(_self, _privptr, _val) \
+                do { \
+                    g_return_val_if_fail (GLUS2_IS_HANDLE (_self), _val); \
+                    _privptr = _self->priv; \
+                    g_return_val_if_fail (_self->handler != NULL, _val); \
+                } while(FALSE)
+
+#define     glus2_ret_false_if_fail_autodef(_self, _privptr) \
+                glus2_ret_val_if_fail_autodef(_self, _privptr, FALSE)
+
+#define     glus2_ret_null_if_fail_autodef(_self, _privptr) \
+                glus2_ret_val_if_fail_autodef(_self, _privptr, NULL)
+
+G_END_DECLS
+
+#endif
